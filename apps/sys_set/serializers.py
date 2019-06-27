564d3fb5d970b8baa38f352b705273c6e2660f4f
zhang <984430071@qq.com>
2019-06-27 16:11:07 +0800

    修改数据库密码

--- apps/sys_set/serializers.py
+++ apps/sys_set/serializers.py
@@ -1,14 +1,14 @@
 from . import models
 from rest_framework import serializers
 from drf_dynamic_fields import DynamicFieldsMixin
-
+from django.contrib.auth.models import Permission
 
 class UserSerializer(DynamicFieldsMixin, serializers.HyperlinkedModelSerializer):
 
     class Meta:
         model = models.UserProfile
         # exclude = []
-        fields = "__all__"
+        fields = ['username', 'first_name', 'last_name']
 
     def create(self, validated_data):
         user = models.UserProfile.objects.create_user(**validated_data)  # 这里新增玩家必须用create_user,否则密码不是秘文
@@ -27,3 +27,10 @@ class DepartmentSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = models.Department
         fields = "__all__"
+
+
+class PermissionSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = Permission
+        fields = ["name", 'codename']
