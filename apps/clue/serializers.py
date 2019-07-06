aefba0215d01f69d64d52a831db327a83b9c5ab3
zhang <984430071@qq.com>
2019-07-06 17:27:59 +0800

    修改clue借口返回信息,增加外键名称

--- apps/clue/serializers.py
+++ apps/clue/serializers.py
@@ -17,6 +17,16 @@ class ChannelSerializer(serializers.HyperlinkedModelSerializer):
 
 
 class ClueSerializer(serializers.ModelSerializer):
+    channel_name = serializers.StringRelatedField(source='channel')
+    follow_up_person_name = serializers.StringRelatedField(source='follow_up_person')
+    auxiliary_status_name = serializers.StringRelatedField(source='auxiliary_status')
+    intended_course_name = serializers.StringRelatedField(source='intended_course')
+    intended_school_name = serializers.StringRelatedField(source='intended_school')
+    plan_school_name = serializers.StringRelatedField(source='plan_school')
+    plan_reception_name = serializers.StringRelatedField(source='plan_reception')
+    plan_teacher_name = serializers.StringRelatedField(source='plan_teacher')
+    plan_course_name = serializers.StringRelatedField(source='plan_course')
+
 
     class Meta:
         model = models.Clue
